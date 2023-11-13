e99f7915c7012df4fa0f16c2f4de279d579e11c6
soran13 <sedaoran13@gmail.com>
2023-11-13 21:22:10 +0100

    custom createProject component created

--- src/app/Dashboard.tsx
+++ src/app/Dashboard.tsx
@@ -1,26 +1,43 @@
 "use client";
-
-import { getUser } from "@/api/user/get-user";
-import { Header } from "@/components/custom/Header";
-import { LatestProjectsView } from "@/components/custom/LatestProjects";
-import { Navigation } from "@/components/custom/Navigation";
-import { useQuery } from "@tanstack/react-query";
-import { useSession } from "next-auth/react";
+import {useState} from 'react';
+import {Header} from "@/components/custom/Header";
+import {LatestProjectsView} from "@/components/custom/LatestProjects";
+import {Navigation} from "@/components/custom/Navigation";
+import {useSession} from "next-auth/react";
+import {Project} from "@/types/types";
+import {useQuery} from "@tanstack/react-query";
+import {getUser} from "@/api/user/get-user";
+import {getProjects} from "@/api/projects/projects";
+import ProjectForm from "@/components/custom/projectForm";
 
 export const Dashboard = () => {
-  const query = useQuery({
-    queryKey: ["user"],
-    queryFn: getUser,
-  });
+    const query = useQuery({
+        queryKey: ["user"],
+        queryFn: getUser,
+    });
+    const session = useSession();
+    const projects = useQuery({
+        queryKey: ["projects"],
+        queryFn: getProjects,
+    })
+    const [selectedProject, setSelectedProject] = useState<Project | null>(null);
+    const [isProjectFormVisible, setProjectFormVisible] = useState(false);
 
-  const session = useSession();
-  return (
-    <div className="flex flex-row top-0 left-0 w-screen h-screen">
-      <Navigation />
-      <section className="w-full overflow-y-auto p-10">
-        <Header />
-        <LatestProjectsView />
-      </section>
-    </div>
-  );
+    const openProjectForm = () => {
+        setProjectFormVisible(true);
+    };
+
+    const closeProjectForm = () => {
+        setProjectFormVisible(false);
+    };
+    return (
+        <div className="flex flex-row top-0 left-0 w-screen h-screen">
+            <Navigation/>
+            <section className="w-full overflow-y-auto p-10">
+                <Header onAddProjectClick={openProjectForm} />
+                {isProjectFormVisible && <ProjectForm onClose={closeProjectForm} />}
+                <LatestProjectsView/>
+            </section>
+        </div>
+    );
 };
--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -1,13 +1,21 @@
-import { AiFillFileAdd } from "react-icons/ai";
+import React from 'react';
+import { AiFillFileAdd } from 'react-icons/ai';
 
-export const Header = () => {
-  return (
-    <div className="flex flex-row justify-between items-center">
-      <div className="text-4xl font-medium">{"Hi _!"}</div>
-      <button className="flex gap-2 items-center bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-700">
-        <AiFillFileAdd />
-        <span>Add Project</span>
-      </button>
-    </div>
-  );
+interface HeaderProps {
+    onAddProjectClick: () => void;
+}
+
+export const Header: React.FC<HeaderProps> = ({ onAddProjectClick }) => {
+    return (
+        <div className="flex flex-row justify-between items-center">
+            <div className="text-4xl font-medium">{"Hi _!"}</div>
+            <button
+                onClick={onAddProjectClick}
+                className="flex gap-2 items-center bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-700"
+            >
+                <AiFillFileAdd />
+                <span>Add Project</span>
+            </button>
+        </div>
+    );
 };
