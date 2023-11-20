5ac420d25cfbfedb4e7419ee293403a1d038084a
rei <reiballa@gmail.com>
2023-11-20 17:24:49 +0100

    improve login register

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,9 +1,132 @@
-import { LoginForm } from "./LoginForm";
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import { ObjectSchema, object, string } from "yup";
+import { useMutation } from "@tanstack/react-query";
+import { useValidatedForm } from "@/hooks/use-validated-form";
+import { useRouter } from "next/navigation";
+import { Alert } from "@/components/ui/alert";
+import { PulseLoader } from "react-spinners";
+import { useEffect } from "react";
+import Link from "next/link";
+
+type LoginState = {
+  username: string;
+  password: string;
+};
+
+const LoginSchema: ObjectSchema<LoginState> = object({
+  username: string().required(),
+  password: string().required(),
+}).required();
 
 export default function Login() {
+  const router = useRouter();
+  const { mutateAsync, isPending, isSuccess, isError, error } = useMutation({
+    mutationFn: async (data: LoginState) => {
+      const res = await signIn("credentials", {
+        username: data.username,
+        password: data.password,
+        redirect: false,
+        callbackUrl: "/",
+      });
+
+      if (res?.ok) {
+        return res;
+      }
+      if (res?.status === 401) {
+        throw res;
+      }
+      throw new Error("An error occurred");
+    },
+  });
+  const { register, handleSubmit } = useValidatedForm({
+    schema: LoginSchema,
+  });
+
+  useEffect(() => {
+    if (isSuccess) {
+      router.push("/");
+    }
+  }, [isSuccess]);
+
+  const onSubmit = async (data: LoginState) => {
+    try {
+      await mutateAsync(data);
+    } catch {
+      //ignore
+    }
+  };
+
   return (
-    <div>
-      <LoginForm />
+    <div
+      className={cn(
+        "w-full grid gap-2 shadow-xs px-4 py-6 border-2 border-gray-100 rounded-xl shadow-gray-300"
+      )}
+    >
+      {isError && <Alert variant="destructive">Invalid Credentials</Alert>}
+
+      <div className="flex flex-col gap-2 items-center">
+        <span className="text-2xl font-semibold">Login to your account</span>
+        <span className="text-sm text-gray-600">
+          Enter your credentials below
+        </span>
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="username">
+              Username
+            </Label>
+            <Input
+              {...register("username")}
+              id="email"
+              placeholder="username"
+              type="text"
+              autoCapitalize="none"
+              autoCorrect="off"
+              disabled={isPending}
+            />
+          </div>
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="password">
+              Password
+            </Label>
+            <Input
+              {...register("password")}
+              id="password"
+              placeholder="Password"
+              type="password"
+              autoCapitalize="none"
+              autoCorrect="off"
+              disabled={isPending}
+            />
+          </div>
+          <Button disabled={isPending}>
+            {isPending && <PulseLoader size={7} color="white" />}
+            {!isPending && <span>Sign In</span>}
+          </Button>
+        </div>
+      </form>
+      <div className="relative text-center">
+        <span className="text-xs ">
+          By clicking continue, you agree to our Terms of Service and Privacy
+          Policy.
+        </span>
+      </div>
+
+      <div className="m-auto">
+        <span className="text-xs text-gray-600">
+          Don't have an account?{" "}
+          <Link href="/auth/signup" className="text-indigo-600 hover:underline">
+            Sign Up
+          </Link>
+        </span>
+      </div>
     </div>
   );
 }
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,9 +1,153 @@
-import { SignUpForm } from "./SignUpForm";
+"use client";
+
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { string, object, ObjectSchema } from "yup";
+import { useValidatedForm } from "@/hooks/use-validated-form";
+import { useMutation } from "@tanstack/react-query";
+import { SignUpRequestParams, signUp } from "@/util/auth/signup";
+import { Alert } from "@/components/ui/alert";
+import Link from "next/link";
+
+type SignUpState = {
+  firstname: string;
+  lastname: string;
+  username: string;
+  password: string;
+};
+
+const signUpSchema: ObjectSchema<SignUpState> = object({
+  firstname: string().required(),
+  lastname: string().required(),
+  username: string().required(),
+  password: string().required(),
+}).required();
 
 export default function Login() {
+  const { mutateAsync, isPending, isSuccess, isError, error } = useMutation({
+    mutationFn: async (data: SignUpRequestParams) => {
+      return signUp(data);
+    },
+  });
+  const { register, handleSubmit } = useValidatedForm({
+    schema: signUpSchema,
+  });
+
+  const onSubmit = async (data: SignUpState) => {
+    try {
+      await mutateAsync(data);
+    } catch {
+      //ignore
+    }
+  };
+
   return (
-    <div>
-      <SignUpForm />
+    <div
+      className={cn(
+        "w-full grid gap-2 shadow-xs px-4 py-6 border-2 border-gray-100 rounded-xl shadow-gray-300"
+      )}
+    >
+      {isError && <Alert>Sign Up Failed</Alert>}
+      {isSuccess ? (
+        <div className="flex flex-col gap-2 items-center">
+          <span className="text-2xl font-semibold">Sign Up Successful</span>
+          <span className="text-sm text-gray-600">Please Login Now</span>
+          <Link href="/auth/login">
+            <Button>Login</Button>
+          </Link>
+        </div>
+      ) : (
+        <>
+          <div className="flex flex-col gap-2 items-center">
+            <span className="text-2xl font-semibold">Create new account</span>
+            <span className="text-sm text-gray-600">
+              Enter your credentials below
+            </span>
+          </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="grid gap-2">
+              <div className="grid gap-1">
+                <Label className="sr-only" htmlFor="firstname">
+                  First Name
+                </Label>
+                <Input
+                  {...register("firstname")}
+                  id="firstname"
+                  placeholder="John"
+                  type="text"
+                  autoCapitalize="none"
+                  autoCorrect="off"
+                  disabled={isPending}
+                />
+              </div>
+              <div className="grid gap-1">
+                <Label className="sr-only" htmlFor="lastname">
+                  Last Name
+                </Label>
+                <Input
+                  {...register("lastname")}
+                  id="lastname"
+                  placeholder="Smith"
+                  type="text"
+                  autoCapitalize="none"
+                  autoCorrect="off"
+                  disabled={isPending}
+                />
+              </div>
+              <div className="grid gap-1">
+                <Label className="sr-only" htmlFor="username">
+                  Username
+                </Label>
+                <Input
+                  {...register("username")}
+                  id="username"
+                  placeholder="username"
+                  type="text"
+                  autoCapitalize="none"
+                  autoCorrect="off"
+                  disabled={isPending}
+                />
+              </div>
+              <div className="grid gap-1">
+                <Label className="sr-only" htmlFor="password">
+                  Password
+                </Label>
+                <Input
+                  {...register("password")}
+                  id="password"
+                  placeholder="Password"
+                  type="password"
+                  autoCapitalize="none"
+                  autoCorrect="off"
+                  disabled={isPending}
+                />
+              </div>
+              <Button disabled={isPending}>
+                {isPending && <div>loading </div>}
+                Sign Up
+              </Button>
+            </div>
+          </form>
+          <div className="relative text-center">
+            <span className="text-xs ">
+              By clicking continue, you agree to our Terms of Service and
+              Privacy Policy.
+            </span>
+          </div>
+        </>
+      )}
+      <div className="m-auto">
+        <div className="text-xs text-gray-600">
+          Already have an account?{" "}
+          <Link href="/auth/login" className="text-indigo-600 hover:underline">
+            Login
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
