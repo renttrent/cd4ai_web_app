827dc74feef9896cbe24db796ffd0016a99ecf75
rei <reiballa@gmail.com>
2023-11-20 17:58:08 +0100

    better forms for login register

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import * as React from "react";
-
 import { cn } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -12,6 +10,9 @@ import { useMutation } from "@tanstack/react-query";
 import { SignUpRequestParams, signUp } from "@/util/auth/signup";
 import { Alert } from "@/components/ui/alert";
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { PulseLoader } from "react-spinners";
 
 type SignUpState = {
   firstname: string;
@@ -28,6 +29,7 @@ const signUpSchema: ObjectSchema<SignUpState> = object({
 }).required();
 
 export default function Login() {
+  const router = useRouter();
   const { mutateAsync, isPending, isSuccess, isError, error } = useMutation({
     mutationFn: async (data: SignUpRequestParams) => {
       return signUp(data);
@@ -45,101 +47,98 @@ export default function Login() {
     }
   };
 
+  useEffect(() => {
+    if (isSuccess) {
+      router.push("/");
+    }
+  }, [isSuccess]);
+
   return (
     <div
       className={cn(
         "w-full grid gap-2 shadow-xs px-4 py-6 border-2 border-gray-100 rounded-xl shadow-gray-300"
       )}
     >
-      {isError && <Alert>Sign Up Failed</Alert>}
-      {isSuccess ? (
-        <div className="flex flex-col gap-2 items-center">
-          <span className="text-2xl font-semibold">Sign Up Successful</span>
-          <span className="text-sm text-gray-600">Please Login Now</span>
-          <Link href="/auth/login">
-            <Button>Login</Button>
-          </Link>
-        </div>
-      ) : (
-        <>
-          <div className="flex flex-col gap-2 items-center">
-            <span className="text-2xl font-semibold">Create new account</span>
-            <span className="text-sm text-gray-600">
-              Enter your credentials below
-            </span>
-          </div>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="grid gap-2">
-              <div className="grid gap-1">
-                <Label className="sr-only" htmlFor="firstname">
-                  First Name
-                </Label>
-                <Input
-                  {...register("firstname")}
-                  id="firstname"
-                  placeholder="John"
-                  type="text"
-                  autoCapitalize="none"
-                  autoCorrect="off"
-                  disabled={isPending}
-                />
-              </div>
-              <div className="grid gap-1">
-                <Label className="sr-only" htmlFor="lastname">
-                  Last Name
-                </Label>
-                <Input
-                  {...register("lastname")}
-                  id="lastname"
-                  placeholder="Smith"
-                  type="text"
-                  autoCapitalize="none"
-                  autoCorrect="off"
-                  disabled={isPending}
-                />
-              </div>
-              <div className="grid gap-1">
-                <Label className="sr-only" htmlFor="username">
-                  Username
-                </Label>
-                <Input
-                  {...register("username")}
-                  id="username"
-                  placeholder="username"
-                  type="text"
-                  autoCapitalize="none"
-                  autoCorrect="off"
-                  disabled={isPending}
-                />
-              </div>
-              <div className="grid gap-1">
-                <Label className="sr-only" htmlFor="password">
-                  Password
-                </Label>
-                <Input
-                  {...register("password")}
-                  id="password"
-                  placeholder="Password"
-                  type="password"
-                  autoCapitalize="none"
-                  autoCorrect="off"
-                  disabled={isPending}
-                />
-              </div>
-              <Button disabled={isPending}>
-                {isPending && <div>loading </div>}
-                Sign Up
-              </Button>
-            </div>
-          </form>
-          <div className="relative text-center">
-            <span className="text-xs ">
-              By clicking continue, you agree to our Terms of Service and
-              Privacy Policy.
-            </span>
-          </div>
-        </>
+      {isError && (
+        // @ts-ignore
+        <Alert variant="destructive">{`Sign Up Failed - ${error.response.data.message}`}</Alert>
       )}
+      <div className="flex flex-col gap-2 items-center">
+        <span className="text-2xl font-semibold">Create new account</span>
+        <span className="text-sm text-gray-600">
+          Enter your credentials below
+        </span>
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="firstname">
+              First Name
+            </Label>
+            <Input
+              {...register("firstname")}
+              id="firstname"
+              placeholder="John"
+              type="text"
+              autoCapitalize="none"
+              autoCorrect="off"
+              disabled={isPending}
+            />
+          </div>
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="lastname">
+              Last Name
+            </Label>
+            <Input
+              {...register("lastname")}
+              id="lastname"
+              placeholder="Smith"
+              type="text"
+              autoCapitalize="none"
+              autoCorrect="off"
+              disabled={isPending}
+            />
+          </div>
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="username">
+              Username
+            </Label>
+            <Input
+              {...register("username")}
+              id="username"
+              placeholder="username"
+              type="text"
+              autoCapitalize="none"
+              autoCorrect="off"
+              disabled={isPending}
+            />
+          </div>
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="password">
+              Password
+            </Label>
+            <Input
+              {...register("password")}
+              id="password"
+              placeholder="Password"
+              type="password"
+              autoCapitalize="none"
+              autoCorrect="off"
+              disabled={isPending}
+            />
+          </div>
+          <Button disabled={isPending}>
+            {isPending && <PulseLoader size={7} color="white" />}
+            {!isPending && <span>Sign Up</span>}
+          </Button>
+        </div>
+      </form>
+      <div className="relative text-center">
+        <span className="text-xs ">
+          By clicking continue, you agree to our Terms of Service and Privacy
+          Policy.
+        </span>
+      </div>
       <div className="m-auto">
         <div className="text-xs text-gray-600">
           Already have an account?{" "}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,9 @@ import { Dashboard } from "./Dashboard";
 export default async function Home() {
   const session = await getServerSession();
 
-  console.log(session);
   if (!session?.user) {
     redirect("/auth/login");
   }
-
-  console.log(process.env.NEXT_PUBLIC_API_URL);
-  console.log(process.env.NEXTAUTH_URL);
-  console.log(process.env.SECRET);
 
   return (
     <main className="">
